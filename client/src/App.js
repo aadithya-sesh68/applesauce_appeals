c43c371bf38e06f161d6aff7893acf5c61c3de6a
Aadithya Seshadri <aadithyaseshadri@Aadithyas-MacBook-Air.local>
2022-01-15 16:07:43 +0530

    Pushed project

---
+++ client/src/App.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Container } from "@material-ui/core";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+
+const App = () => {
+ 
+  return (
+    <BrowserRouter>
+      <Container maxWidth="lg">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+    );
+};
+
+export default App;
---
+++ client/src/constants/actionTypes.js
@@ -0,0 +1,5 @@
+export const CREATE = 'CREATE'
+export const UPDATE = 'UPDATE'
+export const LIKE = 'LIKE'
+export const FETCH_ALL = 'FETCH_ALL'
+export const DELETE = 'DELETE'
